01cc80eb7be24575f5e0ae598c10e1b47c5c5acd
Externius <t.andras@outlook.com>
2026-06-28 20:10:29 +0200

    Fix interceptor + small fixes

--- src/REG.Angular/ClientApp/src/main.ts
+++ src/REG.Angular/ClientApp/src/main.ts
@@ -5,23 +5,39 @@ import {AppComponent} from "./app/app.component";
 import {provideRouter, TitleStrategy} from "@angular/router";
 import {CookieService} from "ngx-cookie-service";
 import {CustomTitleStrategy} from "./app/shared/strategies/custom.title.strategy";
-import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 import {routes} from "./app/app.routing";
 import {provideTranslateService} from "@ngx-translate/core";
 import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
+import {registerLocaleData} from "@angular/common";
+import localeHu from '@angular/common/locales/hu';
+import localeEnGB from '@angular/common/locales/en-GB';
+import {ServerErrorInterceptor} from "./app/core/interceptors/server.error.interceptor";
 
 if (environment.production) {
   enableProdMode();
 }
 
+registerLocaleData(localeEnGB, 'en');
+registerLocaleData(localeHu, 'hu');
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ServerErrorInterceptor,
+      multi: true
+    },
     {provide: TitleStrategy, useClass: CustomTitleStrategy},
+    {provide: 'API_URL', useValue: environment.apiUrl},
     CookieService,
     provideTranslateService({
-      loader: provideTranslateHttpLoader({prefix: "./assets/i18n/", suffix: ".json"}),
+      loader: provideTranslateHttpLoader({
+        prefix: "assets/i18n/",
+        suffix: ".json"
+      }),
       fallbackLang: "en",
       lang: "en"
     }),
--- src/REG.Angular/ClientApp/src/server.ts
+++ src/REG.Angular/ClientApp/src/server.ts
@@ -1,11 +1,44 @@
 import {bootstrapApplication, BootstrapContext} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
-import {provideServerRendering} from '@angular/platform-server';
+import {provideTranslateService} from '@ngx-translate/core';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {provideRouter, TitleStrategy} from "@angular/router";
+import {routes} from "./app/app.routing";
+import {CustomTitleStrategy} from "./app/shared/strategies/custom.title.strategy";
+import {CookieService} from "ngx-cookie-service";
+import {enableProdMode, provideZonelessChangeDetection} from "@angular/core";
+import {registerLocaleData} from "@angular/common";
+import localeHu from '@angular/common/locales/hu';
+import localeEnGB from '@angular/common/locales/en-GB';
+import {environment} from "./environments/environment";
+import {ServerErrorInterceptor} from "./app/core/interceptors/server.error.interceptor";
+
+if (environment.production) {
+  enableProdMode();
+}
+
+registerLocaleData(localeEnGB, 'en');
+registerLocaleData(localeHu, 'hu');
 
 const bootstrap = (context: BootstrapContext) =>
   bootstrapApplication(AppComponent, {
     providers: [
-      provideServerRendering()
+      provideRouter(routes),
+      provideHttpClient(withInterceptorsFromDi()),
+      {
+        provide: HTTP_INTERCEPTORS,
+        useClass: ServerErrorInterceptor,
+        multi: true
+      },
+      {provide: TitleStrategy, useClass: CustomTitleStrategy},
+      {provide: 'API_URL', useValue: environment.apiUrl},
+      CookieService,
+      provideTranslateService({
+        fallbackLang: 'en',
+        lang: 'en'
+      }),
+      provideZonelessChangeDetection()
     ]
   }, context);
+
 export default bootstrap;
